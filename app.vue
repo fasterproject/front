8bb1e5c24c52e6da695e517750641c1e13b47a17
Kenzo <maxesttoujourspasla@gmail.com>
2024-03-23 23:17:54 +0100

    ⚡️ Improve the SEO meta and the Code

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+
+useSeoMeta({
+  titleTemplate: 'Faster Bot',
+  ogImage: 'https://saas-template.nuxt.dev/social-card.png',
+  twitterImage: 'https://saas-template.nuxt.dev/social-card.png',
+  twitterCard: 'summary_large_image'
+})
+
+</script>
+
 <template>
   <div>
   </div>
