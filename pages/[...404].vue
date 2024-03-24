99efb1698a847e6e7dbb265033c8007a59f41139
Kenzo <maxesttoujourspasla@gmail.com>
2024-03-24 18:01:15 +0100

    🚑️ Fix the code

--- pages/[...404].vue
+++ pages/[...404].vue
@@ -11,33 +11,22 @@
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Faster Bot</span>
-            <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <img class="h-8 flex self-center" src="https://cdn.discordapp.com/attachments/1221218737106059326/1221219622271979581/Mark_1_1.png?ex=6611c867&is=65ff5367&hm=376d74424a66fd136bc3567237561e1d622e23c35ed1ae5a3f0fa187fad6d85f&" alt="" />
           </a>
         </div>
-        <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
-        </div>
+        <!--
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
+        -->
       </nav>
       <Dialog as="div" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
         <DialogPanel class="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Faster Bot</span>
-              <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              <img class="h-8" src="https://cdn.discordapp.com/attachments/1221218737106059326/1221219622271979581/Mark_1_1.png?ex=6611c867&is=65ff5367&hm=376d74424a66fd136bc3567237561e1d622e23c35ed1ae5a3f0fa187fad6d85f&" alt="" />
             </a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-              <span class="sr-only">Close menu</span>
-              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
           </div>
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
@@ -66,14 +55,14 @@
           <img class="h-7" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Company name" />
           <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div class="md:grid md:grid-cols-2 md:gap-8">
-              <div>
+              <!--<div>
                 <h3 class="text-sm font-semibold leading-6 text-gray-900">Solutions</h3>
                 <ul role="list" class="mt-6 space-y-4">
                   <li v-for="item in footer.solutions" :key="item.name">
                     <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
                   </li>
                 </ul>
-              </div>
+              </div>--->
               <div class="mt-10 md:mt-0">
                 <h3 class="text-sm font-semibold leading-6 text-gray-900">Support</h3>
                 <ul role="list" class="mt-6 space-y-4">
@@ -113,34 +102,34 @@ import { ref } from 'vue'
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+// const navigation = [
+//  { name: 'Product', href: '#' },
+//  { name: 'Features', href: '#' },
+//  { name: 'Marketplace', href: '#' },
+//  { name: 'Company', href: '#' },
+// ]
 const footer = {
-  solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
-  ],
+  // solutions: [
+    //{ name: 'Marketing', href: '#' },
+    //{ name: 'Analytics', href: '#' },
+    //{ name: 'Commerce', href: '#' },
+    //{ name: 'Insights', href: '#' },
+  //],
   support: [
-    { name: 'Pricing', href: '#' },
+    //{ name: 'Pricing', href: '#' },
     { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
+    //{ name: 'Guides', href: '#' },
     { name: 'API Status', href: '#' },
   ],
   company: [
     { name: 'About', href: '#' },
     { name: 'Blog', href: '#' },
     { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
+    //{ name: 'Press', href: '#' },
+    //{ name: 'Partners', href: '#' },
   ],
   legal: [
-    { name: 'Claim', href: '#' },
+    //{ name: 'Claim', href: '#' },
     { name: 'Privacy', href: '#' },
     { name: 'Terms', href: '#' },
   ],
