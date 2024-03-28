c7ffee44420da3f5c7d3df3d12c01d9e6ff3aea6
Kenzo <maxesttoujourspasla@gmail.com>
2024-03-28 18:50:29 +0100

    ✨ Add the legal part

---
+++ pages/legal/privacy.vue
@@ -0,0 +1,16 @@
+<template>
+    <!--
+      Author: Kenzo (8ruki)
+      Version: v0.1.0
+      Url: Closed Source
+      License(s): MIT
+    -->
+    <div className="flex text-center">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">Privacy Policy</h1>    
+    </div>
+    <div>
+        <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">Last updated: 28 march 2024</p>
+    </div>  
+</template>
+<script setup>
+</script>
