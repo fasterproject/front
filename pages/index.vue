999e1bad42dc99b34ddcd17e4a67910e28cac493
Snowi <154461763+hellosnowi@users.noreply.github.com>
2024-03-30 15:08:05 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,6 @@
           </div>
           <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
             <div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-              <img src="https://cdn.discordapp.com/attachments/1221218737106059326/1223630903130194001/mainimage.png?ex=661a8e15&is=66081915&hm=1233ca37d59f7f01ba2126fa7afb68229389b2dddda94e7974c4cac6028db4b5&" alt="App screenshot" width="2432" height="1442" class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
             </div>
           </div>
         </div>
