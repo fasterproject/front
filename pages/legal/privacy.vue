7141ac149888d0a4569b7ff8d1f17ad7e78a6e2a
Kenzo <maxesttoujourspasla@gmail.com>
2024-03-31 16:31:46 +0200

    🎨 Begin the privacy page

--- pages/legal/privacy.vue
+++ pages/legal/privacy.vue
@@ -9,7 +9,7 @@
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">Privacy Policy</h1>    
     </div>
     <div>
-        <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">Last updated: 28 march 2024</p>
+        <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">Last updated: 28 march 2024</p>    'nuxt-aos'
     </div>  
 </template>
 <script setup>
